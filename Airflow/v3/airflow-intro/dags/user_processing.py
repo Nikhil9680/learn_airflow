ac8c9cf9d66c9743dc8287b586e6320b3908a565
Nikhil9680 <nikhil.kumar@ganitinc.com>
2025-08-02 23:06:18 +0530

    completed the task for user_processing

--- Airflow/v3/airflow-intro/dags/user_processing.py
+++ Airflow/v3/airflow-intro/dags/user_processing.py
@@ -2,7 +2,8 @@ from airflow.sdk import dag, task
 from airflow.providers.common.sql.operators.sql import SQLExecuteQueryOperator
 from airflow.sdk.bases.sensor import PokeReturnValue
 from airflow.providers.standard.operators.python import PythonOperator
-
+from airflow.providers.postgres.hooks.postgres import PostgresHook
+from urllib3.filepost import writer
 
 
 @dag()
@@ -34,5 +35,36 @@ def user_processing():
             condition = False
             fake_user = None
         return PokeReturnValue(is_done=condition, xcom_value=fake_user)
+
+    @task
+    def extract_user(fake_user):
+        return {
+            "id": fake_user["id"],
+            "firstname": fake_user["personalInfo"]["firstName"],
+            "lastname": fake_user["personalInfo"]["lastName"],
+            "email": fake_user["personalInfo"]["email"],
+        }
+
+    @task
+    def process_user(user_info):
+        import csv
+        from datetime import datetime
+        user_info["created_at"] = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        with open("/tmp/user_info.csv", "w", newline="") as f:
+            writer = csv.DictWriter(f, fieldnames=user_info.keys())
+            writer.writeheader()
+            writer.writerow(user_info)
+
+    @task
+    def store_user():
+        hook = PostgresHook(postgres_conn_id='udemy_postgres_connection')
+        hook.copy_expert(
+            sql="COPY users FROM stdin WITH CSV HEADER",
+            filename='/tmp/user_info.csv'
+        )
+
+
+    process_user(extract_user(create_table >> is_api_available())) >> store_user()
+
 
 user_processing()
